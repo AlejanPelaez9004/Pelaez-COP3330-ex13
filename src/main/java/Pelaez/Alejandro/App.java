21b46f69e3e4f862149ffb88188742c810dc9689
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-09-01 14:58:04 -0700

    Completed!

--- src/main/java/Pelaez/Alejandro/App.java
+++ src/main/java/Pelaez/Alejandro/App.java
@@ -8,10 +8,18 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args)
     {
-        System.out.print("What is your name?");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.print("Hello " + name + ", nice to meet you!");
+        System.out.print("What is the principal amount? ");
+        float p = input.nextFloat();
+        System.out.print("What is the rate of interest? ");
+        float r = input.nextFloat();
+        System.out.print("What is the number of years? ");
+        int t = input.nextInt();
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        int n = input.nextInt();
+
+        double a =  (Math.round(p*Math.pow((1+(r/(n*100))), (n*t))*100.0)/100.0);
+        System.out.printf("$%.2f invested at %.1f%% for %d years compounded %d times per year is $%.2f", p, r, t, n, a);
         System.out.print("\n");
     }
 }
